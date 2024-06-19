ab1885921cadc7db618ff36cc1b17ce5849c497f
spyder1211 <soichiro.yano.g@gmail.com>
2024-06-19 09:49:00 +0900

    feat: add prompt template for character analysis

--- Bedrock/serve.py
+++ Bedrock/serve.py
@@ -8,12 +8,17 @@ from langchain_aws import ChatBedrock
 from langserve import add_routes
 
 # 1. Create prompt template
-system_template = "与えられた {特徴}　の人物を想像して、一言コメントしてください:"
+system_template = "与えられた {特徴}　の人物に対して、考察しどんなキャラクターかを箇条書きで列挙してください:"
 prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '名前；{名前}性別：{性別}好きなもの：{好きなもの}嫌いなもの：{嫌いなもの}')
+])
+
+system_template = "与えられた{text}から、その人物がどのような個性を持っているか考慮して、その人らしく一言喋ってください:"
+prompt_template2 = ChatPromptTemplate.from_messages([
     ('system', system_template),
     ('user', '{text}')
 ])
-
 # 2. Create model
 model = ChatBedrock(
     region_name='us-east-1',
@@ -24,7 +29,7 @@ model = ChatBedrock(
 parser = StrOutputParser()
 
 # 4. Create chain
-chain = prompt_template | model | parser
+chain = prompt_template | model | parser | prompt_template2 | model | parser
 
 
 # 4. App definition
