52056e2e18b7a8c7556a34180346445dac9c4972
spyder1211 <soichiro.yano.g@gmail.com>
2024-07-06 18:27:50 +0900

    feat: Update LangChain server with new RSS feed URLs and API endpoint for Bedrock server

---
+++ Tavily/tutorial1.py
@@ -0,0 +1,19 @@
+from langchain import hub
+from langchain.agents import AgentExecutor, create_openai_functions_agent
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+instructions = """You are an assistant."""
+base_prompt = hub.pull("langchain-ai/openai-functions-template")
+prompt = base_prompt.partial(instructions=instructions)
+llm = ChatOpenAI(temperature=0)
+tavily_tool = TavilySearchResults()
+tools = [tavily_tool]
+agent = create_openai_functions_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True,
+)
+output = agent_executor.invoke({"input": "東京都の都知事選挙の結果はどのようになると思いますか？参考にしたソースとともにあなたの考えを教えてください。"})
+print(output)
